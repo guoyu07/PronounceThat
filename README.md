1f0e3a577eb8b775853978adc1a93802a7c64523
Rafael Steil <rafael@insanecorp.com>
2017-01-07 17:31:54 -0200

    Some material design

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # PronounceThat
 Text to speech Android app. You input a word or phrase, and it says it out loud
+
+# Libraries used
+- [Butter knife]()
+- [MaterialEditText](https://github.com/rengwuxian/MaterialEditText)
+- [Ripple effect from Material Design](https://github.com/traex/RippleEffect)
+
+
+http://www.materialdoc.com/
--- app/src/main/java/com/rafaelsteil/pronouncethat/MainActivity.java
+++ app/src/main/java/com/rafaelsteil/pronouncethat/MainActivity.java
@@ -4,12 +4,16 @@ import android.os.Build;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 import android.speech.tts.UtteranceProgressListener;
+import android.speech.tts.Voice;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.Button;
-import android.widget.EditText;
+
+import com.rengwuxian.materialedittext.MaterialEditText;
 
 import java.util.Locale;
+import java.util.Set;
+import java.util.UUID;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -19,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
 	private TextToSpeech tts;
 
 	@BindView(R.id.textField)
-	EditText textField;
+	MaterialEditText textField;
 
 	@BindView(R.id.pronounceButton)
 	Button pronounceButton;
@@ -30,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
 		setContentView(R.layout.activity_main);
 		ButterKnife.bind(this);
 		initTTS();
+		textField.setShowClearButton(true);
 	}
 
 	@Override
@@ -45,7 +50,39 @@ public class MainActivity extends AppCompatActivity {
 	private void initTTS() {
 		tts = new TextToSpeech(this, status -> {
 			if (status != TextToSpeech.ERROR) {
-				tts.setLanguage(Locale.US);
+				int langStatus = tts.setLanguage(Locale.US);
+
+				if (langStatus == TextToSpeech.LANG_NOT_SUPPORTED || langStatus == TextToSpeech.LANG_MISSING_DATA) {
+					// TODO
+				}
+
+				for (TextToSpeech.EngineInfo engine : tts.getEngines()) {
+					Log.d("APP", "Engine => " + engine.name);
+				}
+
+				if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+					try {
+						Set<Voice> voices = tts.getVoices();
+
+						if (voices != null) {
+							for (Voice voice : voices) {
+								Log.d("APP", "Voice => " + voice.getName());
+							}
+						}
+					}
+					catch (Exception e) {
+						Log.w("APP", "Looks like we cannot retrieve available voices. Ignoring it");
+					}
+
+					try {
+						for (Locale language : tts.getAvailableLanguages()) {
+							Log.d("APP", "Language => " + language.getDisplayName());
+						}
+					}
+					catch (Exception e) {
+						Log.w("APP", "Looks like we cannot retrieve available languages. Ignoring it");
+					}
+				}
 			}
 			else {
 				Log.e("APP", "Error on TTS init. status=" + status);
@@ -85,7 +122,7 @@ public class MainActivity extends AppCompatActivity {
 			return;
 		}
 
-		String utteranceId = String.valueOf(System.currentTimeMillis());
+		String utteranceId = UUID.randomUUID().toString();
 
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
 			tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, utteranceId);
